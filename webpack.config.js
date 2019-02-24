21e3fabf0a723aa461359c5d6718b052e643adc9
Your Name <you@example.com>
2019-02-24 21:49:35 +0000

    Added minimum encore config and set up notifications.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // the project directory where all compiled assets will be stored
+    .setOutputPath('public/build')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    .addEntry('rep_log', './public/assets/js/RepLogApp.js')
+
+    .enableBuildNotifications()
+;
+
+// exports the final config
+module.exports = Encore.getWebpackConfig();
